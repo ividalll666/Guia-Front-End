47c23961a700255eb55d728c5b8b3bcc0fcb0d6e
ividalll666 <48143354+ividalll666@users.noreply.github.com>
2020-04-20 23:51:01 -0300

    Arquivos da primeira página do site.
    Primeira versão.

---
+++ index.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+	<link rel="stylesheet" type="text/css" href="_css/estilo.css">
+
+	<meta charset="utf-8">
+
+	<title> Guia! HTML e CSS. </title>
+
+</head>
+
+<body>
+
+	<div id="interface">
+			<header>
+							<nav>
+										<ul class="listanav">
+											
+											<li class="navbar inicio"> 	 <a class="navbarlink" href="pagina1.html">Início</a> </li>
+											<li class="navbar fotos">  	 <a class="navbarlink" href="pagina2.html">Fotos</a> </li>
+											<li class="navbar MConheca"> <a class="navbarlink" href="pagina3.HTML">Me conheça</a> </li>
+
+										</ul>
+							</nav>
+
+							
+
+							<div id="painel">
+									
+									<hgroup id = "titulos">
+											<h1> Guia de HTML CSS e JS!</h1>
+
+											<h2> Com muito carinho e muito esforço. </h2>
+									</hgroup>
+							</div>
+
+			</header>	
+
+			<section id="secao" >
+							
+							<article id="main">
+									<h1> Minha Apostila - Esclarecimentos </h1>
+
+									<p> Bom, depois de muito tempo querendo fazer alguma coisa decente na minha vida, reuni minhas forças para estudar. Fiquei pensando um bom tempo em que e no que centrar todo meu foco. Depois de analisar tudo o que tinha a minha volta e identificar o que eu gosto na grade escolar, percebi que na área da programação me seria bem útil. Sempre gostei muito de costomizar as coisas e modificar elas ao meu gosto. Lembro das aulas de HTML e CSS no meu primeiro ano do curso técnico. Grande pequeno Prof. João. Achava muito legal e o código parecia ser bem bonito, coisa que me enche os olhos (bobagens, eu sei). Com o passar do tempo e do curso fui aprendendo outras coisas. Gostei muito de C# e comecei a me aventurar por lá e realmente fiz algumas coisinhas, mas eu sentia que tava faltando alguma coisa.</p>
+
+									<p> Tinha deixado HTML e toda essa parte de WEB para lá, mas sentia falta. Quarentena nossa veio e eu comecei a ficar entediado em casa. Com o passar dos dias eu fiquei completamente sem o que fazer e pensei: "por que não reviver um amor esquecido?". Sim, eu sei que normalmente essa frase não vem seguida de uma coisa inteligente, mas dessa vez, pelo menos dessa vez, deu bom! Comecei a ler algumas coisas, ver alguns artigos, olhei video aula ali e aqui, e fui me interessando cada vez mais. Para mim sempre foi muito difícil estudar em casa, então isso seria um desafio também. Decidi que tudo o que eu fosse fazer, eu iria separar, comentar, explicar e documentar. Desde meu primeiro encontro com as TAGs, o que elas fazem, até passando por propriedades de CSS, depois vendo JS e tudo mais. O básico eu já vi e vi bem, no meu curso graças ao meu professor da época. Mas sempre há aquilo que esquece, ou não pegou direito. Então o meu objetivo passou a ser me aprofundar.  </p>
+
+									<p> Eu queria e ainda quero me aprofundar cada vez mais a ponto de conseguir fazer um site sobre qualquer coisa, e deixá-lo bonito e funcional. Minha meta é poder trabalhar pelo menos para tirar um dinheirinho com alguns freela aqui e ali. Pela primeira vez em sempre, eu quis realmente estudar e enquanto eu estudo, sinto que irá fazer a diferença. Neste site você verá sites que eu fiz para estudo, minha apostila com tudo o que eu fiz até hoje, com ela sendo atualizada constantemente (pelo menos vou tentar) e quem sabe, um dia, trabalhos que eu fiz parte. Quero atualizar o Layout deste site aqui também a medida que eu vá aprendendo.</p>
+							</article>
+			</section>
+
+
+			<aside id="lateral">
+
+				<div id="lmain">
+									<h1> Aproveite! </h1>
+
+									<div id="container-foto">
+
+										<ul id="y">
+											<li> 
+												<span> <img id="imagem" src="_imagem/camada5.jpg"> </span>
+												<span> <img id="imagem" src="_imagem/camada4.jpg"> </span>
+												<span> <img id="imagem" src="_imagem/camada3.jpg"> </span>
+												<span> <img id="imagem" src="_imagem/camada2.jpg"> </span>
+												<span> <img id="imagem" src="_imagem/camada1.jpg"> </span>
+											</li>
+										</ul>
+									</div>
+									
+									<h1> Link para download da apostila</h1>
+
+									<p> Esse será um link que irá compartilhar a apostila pelo meu google drive. Tudo o que precisa é um leitor de PDF, já que ela está convertida nesse formato. Espero que não tenham problemas para lê-la.</p>
+									<p> <a href=""> [linkaqui] </a></p>
+				</div>
+			</aside>
+			
+			<footer id="rodape">
+									
+									<p> <span id="sociais"> Me segue lá para a gente trocar umas ideias: </span> <br> Insta: vidalitallo </p> 
+									<p> 2020 </p>
+
+			</footer>		
+
+	</div>
+</body>
+</html>
